3730cb133fe9940f4f682e2e24c23c5ca4817d08
COLIN_GARMS <colin.garms@online.de>
2022-05-26 12:30:33 +0200

    background, laser, floor, ceiling

--- UnityProjekt/JetpackRider/Assets/Scripts/spawning/Coins.cs
+++ UnityProjekt/JetpackRider/Assets/Scripts/spawning/Coins.cs
@@ -1,26 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Coins : MonoBehaviour
 {
     public float speed = -3.0f;
     private Rigidbody2D rb;
-    private Vector2 screenBounds;
+    private Vector3 coinPos;
+    
+    public void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+        if (transform.parent != null)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(speed, 0);
-        screenBounds = Camera.main.ScreenToViewportPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
     {
+        coinPos = Camera.main.WorldToScreenPoint(transform.position);
         rb.velocity = new Vector2(speed, 0);
-        if (transform.position.x <-10)
-        {
-            Destroy(this.gameObject);
-        }
+        
     }
 }
