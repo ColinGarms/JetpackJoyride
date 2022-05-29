db19f6cb4be86c4edafebb618804a359f608aa04
COLIN_GARMS <colin.garms@online.de>
2022-05-30 01:35:39 +0200

    deleted Sprites and Design for different levels

--- UnityProjekt/JetpackRider/Assets/Scripts/General/LevelSelection.cs
+++ UnityProjekt/JetpackRider/Assets/Scripts/General/LevelSelection.cs
@@ -4,6 +4,10 @@ namespace General
 {
     public class LevelSelection :MonoBehaviour
     {
+        [SerializeField] private GameObject level1;
+        [SerializeField] private GameObject level2;
+        [SerializeField] private GameObject level3;
+        
         public void setLevel(int level)
         {
             publicInformation.level = level;
@@ -11,14 +15,26 @@ namespace General
         public void setLevel1()
         {
             setLevel(1);
+            level1.SetActive(true);
+            level2.SetActive(false);
+            level3.SetActive(false);
+            
         }
         public void setLevel2()
         {
             setLevel(2);
+            level1.SetActive(false);
+            level2.SetActive(true);
+            level3.SetActive(false);
         }
         public void setLevel3()
         {
             setLevel(3);
+            level1.SetActive(false);
+            level2.SetActive(false);
+            level3.SetActive(true);
         }
+        
+        
     }
 }
